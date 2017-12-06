ffd130712e5605e2a2f8b0780ed077aa720c98a2
Romain Tissier <romain.tissier@telecomnancy.net>
2017-12-06 19:02:06 +0100

    Shell command from C test

--- C/Shell/try_env.c
+++ C/Shell/try_env.c
@@ -5,6 +5,7 @@
  * @date 12/2017
  */
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * @brief Entry point of the program
@@ -12,6 +13,12 @@
  */
 int main(){
 	printf("Try environment program\n");
+
+	// We can see that we are not in the same environment
+	// The $MYVAR variable is not printed
+	system("echo \"Try to print some variable :\"");
+	system("MYVAR=coucou");
+	system("echo $MYVAR");
 
 	// Return success
 	return 0; 
