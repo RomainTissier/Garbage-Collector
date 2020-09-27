410c28975d39af540b1ad46fb50a7a6f69a9487e
RomainTissier <romain.tissier@telecomnancy.net>
2020-09-27 12:07:54 +0200

    Add a cpp code checks script

---
+++ Cpp/lint.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#
+# Build a file with different compiler and execute some code checks
+#
+
+echo "Call gcc"
+g++ -c $1 \
+	-Wall \
+       	-Wextra \
+       	-Wshadow \
+       	-Wnon-virtual-dtor \
+       	-pedantic \
+       	-Wpedantic \
+       	-Wold-style-cast \
+	-Wcast-align \
+	-Wunused \
+	-Woverloaded-virtual \
+	-Wconversion \
+	-Wsign-conversion \
+	-Wmisleading-indentation \
+	-Wduplicated-cond \
+	-Wduplicated-branches \
+	-Wlogical-op \
+	-Wnull-dereference \
+	-Wuseless-cast \
+	-Wdouble-promotion \
+	-Weffc++
+
+echo "Call clang"
+clang -c $1 \
+	-Wall \
+       	-Wextra \
+       	-Wshadow \
+       	-Wnon-virtual-dtor \
+       	-pedantic \
+       	-Wpedantic \
+       	-Wold-style-cast \
+	-Wcast-align \
+	-Wunused \
+	-Woverloaded-virtual \
+	-Wconversion \
+	-Wsign-conversion \
+	-Wmisleading-indentation \
+	-Wnull-dereference \
+	-Wdouble-promotion \
+	-Weffc++ \
+        -Weverything # May be too much...
+
+echo "Call cppcheck"
+cppcheck --enable=all $1
+
+echo "Call clang-tidy"
+clang-tidy --checks="*" $1 --
