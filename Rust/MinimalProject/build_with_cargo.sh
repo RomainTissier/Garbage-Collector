112fc83e82193712e67b47de2541bf41d4782549
RomainTissier <romain.tissier@telecomnancy.net>
2020-10-03 17:42:24 +0200

    Add rust minimal project example

---
+++ Rust/MinimalProject/build_with_cargo.sh
@@ -0,0 +1,8 @@
+# Format files
+cargo fmt
+
+# Lint
+cargo clippy
+
+# Build
+cargo build
---
+++ Rust/MinimalProject/src/main.rs
@@ -0,0 +1,43 @@
+//! This file contains the main function
+
+#![warn(
+    missing_docs,
+    absolute_paths_not_starting_with_crate,
+    anonymous_parameters,
+    box_pointers,
+    clashing_extern_declarations,
+    deprecated_in_future,
+    elided_lifetimes_in_paths,
+    explicit_outlives_requirements,
+    indirect_structural_match,
+    keyword_idents,
+    macro_use_extern_crate,
+    meta_variable_misuse,
+    missing_copy_implementations,
+    missing_crate_level_docs,
+    missing_debug_implementations,
+    missing_docs,
+    missing_doc_code_examples,
+    non_ascii_idents,
+    private_doc_tests,
+    single_use_lifetimes,
+    trivial_casts,
+    trivial_numeric_casts,
+    unaligned_references,
+    unreachable_pub,
+    unsafe_code,
+    unstable_features,
+    unused_crate_dependencies,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_lifetimes,
+    unused_qualifications,
+    unused_results,
+    variant_size_differences
+)] // unsafe_op_in_unsafe_fn is unstable
+#![warn(clippy::all)]
+
+/// Entry point of the program
+fn main() {
+    println!("Hey!");
+}
